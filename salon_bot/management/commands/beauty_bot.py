c07d48d96a607d63c64b46cb08a5a932c435662e
Maxim-Pekov <max.pekov@gmail.com>
2022-12-08 18:55:24 +0300

    create admin

--- salon_bot/management/commands/beauty_bot.py
+++ salon_bot/management/commands/beauty_bot.py
@@ -1,12 +1,12 @@
 import os
 import logging
 from pathlib import Path
-from pprint import pprint
-
+from django.core.management.base import BaseCommand
 from telegram import Update, Bot
 from dotenv import load_dotenv
-
+from ...models import User, Service, Specialist, Salon, Purchase
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
+from telegram.utils.request import Request
 from telegram.ext import (
     Updater,
     CommandHandler,
@@ -14,21 +14,13 @@ from telegram.ext import (
     ConversationHandler,
     CallbackContext,
 )
+
 saloon = ''
 master = ''
 service = ''
 
 load_dotenv()
 TOKEN_TG = os.getenv('TOKEN_TG')
-
-bot = Bot(token=TOKEN_TG, )
-updates = bot.get_updates()
-
-# Enable logging
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
-)
-
 logger = logging.getLogger(__name__)
 
 # Stages
@@ -78,13 +70,8 @@ TITLES = {
 
 def start(update: Update, context: CallbackContext) -> int:
     """Send message on `/start`."""
-    # Get user that sent /start and log his name
     user = update.message.from_user
     logger.info("User %s started the conversation.", user.first_name)
-    # Build InlineKeyboard where each button has a displayed text
-    # and a string as callback_data
-    # The keyboard is a list of button rows, where each row is in turn
-    # a list (hence `[[...]]`).
 
     keyboard = [
         [
@@ -95,9 +82,7 @@ def start(update: Update, context: CallbackContext) -> int:
         ],
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
-    # Send message with text and appended InlineKeyboard
     update.message.reply_text("Что вы хотите сделать?", reply_markup=reply_markup)
-    # Tell ConversationHandler that we're in state `FIRST` now
     return FIRST
 
 
@@ -110,14 +95,11 @@ def start_over(update: Update, context: CallbackContext) -> int:
     query.answer()
     keyboard = [
         [
-            InlineKeyboardButton("1", callback_data=str(ONE)),
-            InlineKeyboardButton("2", callback_data=str(TWO)),
+            InlineKeyboardButton(TITLES['callback_service'], callback_data=CALLBACK_SERVICE),
+            InlineKeyboardButton(TITLES['personal_area'], callback_data=PERSONAL_AREA),
         ]
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
-    # Instead of sending a new message, edit the message that
-    # originated the CallbackQuery. This gives the feeling of an
-    # interactive menu.
     query.edit_message_text(text="Что вы хотите сделать?", reply_markup=reply_markup)
     return FIRST
 
@@ -221,6 +203,7 @@ def four(update: Update, context: CallbackContext) -> int:
     )
     return CHOICE_MASTER
 
+
 def five(update: Update, context: CallbackContext) -> int:
     """Show new choice of buttons"""
     query = update.callback_query
@@ -265,58 +248,117 @@ def publish_photo(update: Update, context: CallbackContext):
         bot.send_document(chat_id=update.effective_chat.id, document=img_path)
 
 
-def main() -> None:
-    """Run the bot."""
-    # Create the Updater and pass it your bot's token.
-    updater = Updater(TOKEN_TG)
+class Command(BaseCommand):
+    help = 'Команда настройки Telegram-бота в приложении Django.'
 
-    # Get the dispatcher to register handlers
-    dispatcher = updater.dispatcher
-
-    # ^ means "start of line/string"
-    # $ means "end of line/string"
-    # So ^ABC$ will only allow 'ABC'
-    conv_handler = ConversationHandler(
-        entry_points=[CommandHandler('start', start)],
-        states={
-            FIRST: [
-                CallbackQueryHandler(one, pattern='^' + CALLBACK_SERVICE + '$', pass_user_data=True),
-                CallbackQueryHandler(two, pattern='^' + PERSONAL_AREA + '$', pass_user_data=True),
-                CallbackQueryHandler(three, pattern='^' + CHOICE_SALOON + '$', pass_user_data=True),
-                CallbackQueryHandler(four, pattern='^' + CHOICE_MASTER + '$', pass_user_data=True),
-                CallbackQueryHandler(five, pattern='^' + CHOICE_SERVICE + '$', pass_user_data=True),
-            ],
-            CHOICE_MASTER: [
-                # CallbackQueryHandler(start_over, pattern='^' + str(ONE) + '$'),
-                CallbackQueryHandler(five, pass_user_data=True),
-            ],
-            CHOICE_SALOON: [
-                # CallbackQueryHandler(start_over, pattern='^' + str(ONE) + '$'),
-                CallbackQueryHandler(four, pass_user_data=True),
-            ],
-            CHOICE_SERVICE: [
-                # CallbackQueryHandler(start_over, pattern='^' + str(ONE) + '$'),
-                CallbackQueryHandler(three, pass_user_data=True),
-            ],
-            END: [
-                # CallbackQueryHandler(start_over, pattern='^' + str(ONE) + '$'),
-                CallbackQueryHandler(end,),
-            ],
-        },
-        fallbacks=[CommandHandler('start', start)],
-    )
-
-    # Add ConversationHandler to dispatcher that will be used for handling updates
-    dispatcher.add_handler(conv_handler)
-
-    # Start the Bot
-    updater.start_polling()
-
-    # Run the bot until you press Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
-    updater.idle()
+    def handle(self, *args, **kwargs):
+        request = Request(
+            connect_timeout=0.5,
+            read_timeout=1.0,
+        )
+        bot = Bot(
+            request=request,
+            token=TOKEN_TG,
+        )
+        print(bot.get_me())
+        updater = Updater(TOKEN_TG)
+        dispatcher = updater.dispatcher
+        conv_handler = ConversationHandler(
+            entry_points=[CommandHandler('start', start)],
+            states={
+                FIRST: [
+                    CallbackQueryHandler(one, pattern='^' + CALLBACK_SERVICE + '$', pass_user_data=True),
+                    CallbackQueryHandler(two, pattern='^' + PERSONAL_AREA + '$', pass_user_data=True),
+                    CallbackQueryHandler(three, pattern='^' + CHOICE_SALOON + '$', pass_user_data=True),
+                    CallbackQueryHandler(four, pattern='^' + CHOICE_MASTER + '$', pass_user_data=True),
+                    CallbackQueryHandler(five, pattern='^' + CHOICE_SERVICE + '$', pass_user_data=True),
+                ],
+                CHOICE_MASTER: [
+                    CallbackQueryHandler(five, pass_user_data=True),
+                ],
+                CHOICE_SALOON: [
+                    CallbackQueryHandler(four, pass_user_data=True),
+                ],
+                CHOICE_SERVICE: [
+                    CallbackQueryHandler(three, pass_user_data=True),
+                ],
+                END: [
+                    CallbackQueryHandler(end, ),
+                ],
+            },
+            fallbacks=[CommandHandler('start', start)],
+        )
+        dispatcher.add_handler(conv_handler)
+        updater.start_polling()
+        updater.idle()
 
 
-if __name__ == '__main__':
-    main()
+
+# bot = Bot(token=TOKEN_TG, )
+# updates = bot.get_updates()
+#
+# # Enable logging
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+)
+
+
+
+
+
+
+# def main() -> None:
+#     """Run the bot."""
+#     # Create the Updater and pass it your bot's token.
+#     updater = Updater(TOKEN_TG)
+#
+#     # Get the dispatcher to register handlers
+#     dispatcher = updater.dispatcher
+#
+#     # ^ means "start of line/string"
+#     # $ means "end of line/string"
+#     # So ^ABC$ will only allow 'ABC'
+#     conv_handler = ConversationHandler(
+#         entry_points=[CommandHandler('start', start)],
+#         states={
+#             FIRST: [
+#                 CallbackQueryHandler(one, pattern='^' + CALLBACK_SERVICE + '$', pass_user_data=True),
+#                 CallbackQueryHandler(two, pattern='^' + PERSONAL_AREA + '$', pass_user_data=True),
+#                 CallbackQueryHandler(three, pattern='^' + CHOICE_SALOON + '$', pass_user_data=True),
+#                 CallbackQueryHandler(four, pattern='^' + CHOICE_MASTER + '$', pass_user_data=True),
+#                 CallbackQueryHandler(five, pattern='^' + CHOICE_SERVICE + '$', pass_user_data=True),
+#             ],
+#             CHOICE_MASTER: [
+#                 # CallbackQueryHandler(start_over, pattern='^' + str(ONE) + '$'),
+#                 CallbackQueryHandler(five, pass_user_data=True),
+#             ],
+#             CHOICE_SALOON: [
+#                 # CallbackQueryHandler(start_over, pattern='^' + str(ONE) + '$'),
+#                 CallbackQueryHandler(four, pass_user_data=True),
+#             ],
+#             CHOICE_SERVICE: [
+#                 # CallbackQueryHandler(start_over, pattern='^' + str(ONE) + '$'),
+#                 CallbackQueryHandler(three, pass_user_data=True),
+#             ],
+#             END: [
+#                 # CallbackQueryHandler(start_over, pattern='^' + str(ONE) + '$'),
+#                 CallbackQueryHandler(end,),
+#             ],
+#         },
+#         fallbacks=[CommandHandler('start', start)],
+#     )
+#
+#     # Add ConversationHandler to dispatcher that will be used for handling updates
+#     dispatcher.add_handler(conv_handler)
+#
+#     # Start the Bot
+#     updater.start_polling()
+#
+#     # Run the bot until you press Ctrl-C or the process receives SIGINT,
+#     # SIGTERM or SIGABRT. This should be used most of the time, since
+#     # start_polling() is non-blocking and will stop the bot gracefully.
+#     updater.idle()
+#
+#
+# if __name__ == '__main__':
+#     main()
